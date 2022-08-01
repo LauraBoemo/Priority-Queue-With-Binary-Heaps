50449e948099a971e0dd83a8373e2585e47e02c7
Laura Boemo <lauraboemo@gmail.com>
2022-08-01 00:52:13 -0300

    Final Adjustments

--- listaPriorizada.c
+++ listaPriorizada.c
@@ -40,54 +40,60 @@ EstruturaDoHeap* inicializarHeap() {
 }
 
 void trocarElementos(int *a, int *b) {
-  int temp = *b;
-  *b = *a;
-  *a = temp;
+    int temp = *b;
+    *b = *a;
+    *a = temp;
 }
 
 void ordenarHeap(EstruturaDoHeap* h, int i) {
-  if (h->size > 1) {
-    int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
-    
-    if (l < h->size && h->heaparray[l] > h->heaparray[largest])
-      largest = l;
-    if (r < h->size && h->heaparray[r] > h->heaparray[largest])
-      largest = r;
-
-    if (largest != i) {
-      trocarElementos(&h->heaparray[i], &h->heaparray[largest]);
-      ordenarHeap(h, largest);
+    if (h->size > 1) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        
+        if (l < h->size && h->heaparray[l] > h->heaparray[largest])
+            largest = l;
+        if (r < h->size && h->heaparray[r] > h->heaparray[largest])
+            largest = r;
+        
+        if (largest != i) {
+            trocarElementos(&h->heaparray[i], &h->heaparray[largest]);
+            ordenarHeap(h, largest);
+        }
     }
-  }
 }
 
 void inserirNoHeap(EstruturaDoHeap* h, int newNum) {
-  if (h->size != 0) {
-    for (int i = h->size - 1; i >= 0; i--) {
-      ordenarHeap(h, i);
+    if (h->size != 0) {
+        for (int i = (((h->size)/2) - 1); i >= 0; i--) {
+            ordenarHeap(h, i);
+        }
     }
-  }
-  
-  h->heaparray[h->size] = newNum;
-  h->size += 1;
+
+    h->heaparray[h->size] = newNum;
+    h->size += 1;
 }
 
 void removerElementoDoHeap(EstruturaDoHeap* h, int num) {
-  int i;
-  for (i = 0; i < h->size; i++) {
-    if (num == h->heaparray[i])
-      break;
-  }
-
-  trocarElementos(&h->heaparray[i], &h->heaparray[h->size - 1]);
-  h->size -= 1;
-  for (int i = h->size - 1; i >= 0; i--) {
-    ordenarHeap(h, i);
-  }
+    int i;
+    for (i = 0; i < h->size; i++) {
+        if (num == h->heaparray[i])
+            break;
+    }
+    
+    trocarElementos(&h->heaparray[i], &h->heaparray[h->size - 1]);
+    h->size -= 1;
+    
+    for (int i = (((h->size)/2) - 1); i >= 0; i--) {
+        ordenarHeap(h, i);
+    }
 }
 
+void imprimirArray(EstruturaDoHeap* h) {
+    for (int i = 0; i < h->size; ++i) {
+        printf("%d ", h->heaparray[i]);
+    }
+}
 
 void imprimirHeap(EstruturaDoHeap* h) {
     int contadorLinhas = 2, auxContador = 0;
