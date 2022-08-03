c514653a21c73798655cf39892f77a73dcc1aa0e
Laura Boemo <lauraboemo@gmail.com>
2022-08-02 21:46:55 -0300

    Update listaPriorizada.c

--- listaPriorizada.c
+++ listaPriorizada.c
@@ -41,7 +41,7 @@ EstruturaDoHeap* inicializarHeap() {
     h->size = 0;
 }
 
-void inverterNosDoHeap(int *a, int *b) {
+void trocarElementos(int *a, int *b) {
     int temp = *b;
     *b = *a;
     *a = temp;
@@ -53,13 +53,13 @@ void ordenarHeap(EstruturaDoHeap* h, int i) {
         int l = 2 * i + 1;
         int r = 2 * i + 2;
         
-        if (h->heaparray[l] > h->heaparray[largest])
+        if (l < h->size && h->heaparray[l] > h->heaparray[largest])
             largest = l;
-        if (h->heaparray[r] > h->heaparray[largest])
+        if (r < h->size && h->heaparray[r] > h->heaparray[largest])
             largest = r;
         
         if (largest != i) {
-            inverterNosDoHeap(&h->heaparray[i], &h->heaparray[largest]);
+            trocarElementos(&h->heaparray[i], &h->heaparray[largest]);
             ordenarHeap(h, largest);
         }
     }
@@ -75,7 +75,6 @@ void inserirNoHeap(EstruturaDoHeap* h, int newNum) {
     h->heaparray[h->size] = newNum;
     h->size += 1;
     
-    
     for (int i = (((h->size)/2) - 1); i >= 0; i--) {
         ordenarHeap(h, i);
     }
@@ -83,16 +82,15 @@ void inserirNoHeap(EstruturaDoHeap* h, int newNum) {
 
 void removerElementoDoHeap(EstruturaDoHeap* h, int num) {
     int i;
-    
     for (i = 0; i < h->size; i++) {
         if (num == h->heaparray[i])
             break;
     }
     
-    inverterNosDoHeap(&h->heaparray[i], &h->heaparray[h->size]);
+    trocarElementos(&h->heaparray[i], &h->heaparray[h->size - 1]);
     h->size -= 1;
     
-    for (int i = (h->size); i >= 0; i--) {
+    for (int i = (((h->size)/2) - 1); i >= 0; i--) {
         ordenarHeap(h, i);
     }
 }
