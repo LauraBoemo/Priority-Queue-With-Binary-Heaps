cc4090814ec2075ef41ae67f4c410381b0d56c2e
Laura Boemo <lauraboemo@gmail.com>
2022-08-22 21:29:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main() {
         printf("O que desejas fazer?\n\n1. Inserir valor no Heap.\n2. Remover valor do Heap.\n3. Imprimir o Heap\n4. Sair do Programa\n\nDigite o numero desejado: ");
         scanf("%i", &repostaUsuario);
         rotearFuncoes(h, repostaUsuario);
-    } 
+    }  
     
     free(h);
     system("PAUSE");
